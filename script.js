2c3f3eb5de8827e939c07721053672e5ea0ccebc
Dhivya-Sivagnanam <132328108+Dhivya-Sivagnanam@users.noreply.github.com>
2023-09-09 19:03:56 +0530

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Password validation function
+
     function isStrongPassword(password) {
         const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
         return regex.test(password);
     }
 
-    // Validation for strong password
     const passwordInput = document.getElementById("password");
     const passwordError = document.getElementById("passwordError");
 
@@ -22,7 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
     
    
 
-    // Registration form submission
     const registerForm = document.getElementById("registerForm");
     const registrationMessage = document.getElementById("registrationMessage");
 
@@ -32,14 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
         const password = registerForm.elements.password.value;
 
         if (isStrongPassword(password)) {
-            // Implement your registration logic here (e.g., store in a database)
-
-            // Show registration success message and link to login page
+           
             registrationMessage.textContent = "Registration successful!";
             registrationMessage.style.color = "green";
             setTimeout(function () {
                 window.location.href = "login.html";
-            }, 3000); // Redirect to login after 3 seconds
+            }, 3000);
         } else {
             registrationMessage.textContent = "Please fix the password issues.";
             registrationMessage.style.color = "red";
@@ -47,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 document.addEventListener("DOMContentLoaded", function () {
-    // Login form submission
+    
     const loginForm = document.getElementById("loginForm");
 
     loginForm.addEventListener("submit", function (event) {
@@ -55,13 +51,11 @@ document.addEventListener("DOMContentLoaded", function () {
         const username = loginForm.elements.username.value;
         const password = loginForm.elements.password.value;
 
-        // Implement your login logic here (e.g., check against a database)
-        // For this example, let's assume valid credentials are:
         const validUsername = "Dhivya";
         const validPassword = "Dhivya@2003";
 
         if (username === validUsername && password === validPassword) {
-            // Redirect to secure.html on successful login
+          
             window.location.href = "secure.html";
         } else {
             alert("Invalid username or password. Please try again.");
